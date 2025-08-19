16f6ed61da0d396ccbb251f16fd1fbe15f7c0fc0
SeungHyeok Yun <162292720+SeungHyeokYoon@users.noreply.github.com>
2025-08-19 22:07:49 +0900

    [Gold V] Title: 모독, Time: 328 ms, Memory: 27724 KB -BaekjoonHub

---
+++ 백준/Gold/16678. 모독/모독.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        int target = 1;
+
+        for(int i = 0; i<n;  i++){
+            if(arr[i] < target) continue;
+            sum += arr[i] - target;
+            target++;
+        }
+
+        System.out.println(sum);
+
+    }
+    
+
+
+}
