cf2959015e34e92c015be92847675e46932a555c
SeungHyeok Yun <162292720+SeungHyeokYoon@users.noreply.github.com>
2025-09-03 11:30:49 +0900

    [Gold I] Title: 전구를 켜라, Time: 416 ms, Memory: 28944 KB -BaekjoonHub

--- 백준/Gold/2423. 전구를 켜라/전구를 켜라.java
+++ 백준/Gold/2423. 전구를 켜라/전구를 켜라.java
@@ -10,10 +10,10 @@ public class Main {
     static int[][] dp;
 
     static class Node implements Comparable<Node>{
-        int x, y, cnt, type;
+        int x, y, cnt;
 
-        Node(int x, int y, int cnt, int type){
-            this.x = x; this.y = y; this.cnt = cnt; this.type = type;
+        Node(int x, int y, int cnt){
+            this.x = x; this.y = y; this.cnt = cnt;
         }
 
         @Override
@@ -38,7 +38,7 @@ public class Main {
         for(int i = 0; i<n; i++){
             String str = br.readLine();
             for(int j = 0; j<m; j++){
-                map[i][j] = (str.charAt(j) == '/') ? 1 : 0;
+                map[i][j] = ((str.charAt(j) == '/' && (i+j)%2 == 1) || (str.charAt(j) == '\\' && (i+j)%2 == 0)) ? 0 : 1;
                 dp[i][j] = Integer.MAX_VALUE;
             }
         }
@@ -50,44 +50,28 @@ public class Main {
 
     static void dijkstra(){
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        dp[0][0] = map[0][0]==0 ? 0 : 1;
-        pq.add(new Node(0, 0, (map[0][0]==1 ? 1 : 0), 0));
+        dp[0][0] = map[0][0];
+        pq.add(new Node(0, 0, dp[0][0]));
 
         while(!pq.isEmpty()){
             Node now = pq.poll();
 
-            if(now.cnt > dp[now.y][now.x]) continue;
+            if(dp[now.y][now.x] < now.cnt) continue;
 
-            for(int i = 0; i<4; i++){
-                int nx = now.x + dx[now.type][i];
-                int ny = now.y + dy[now.type][i];
-
-                if(nx>=0 && ny>=0 && nx<m && ny<n){
-                    int add = (now.type==map[ny][nx]) ? 1 : 0;
-                    if(dp[ny][nx] > now.cnt + add){
-                        dp[ny][nx] = now.cnt + add;
-                        if(nx == m-1 && ny == n-1) return;
-                        pq.add(new Node(nx, ny, now.cnt+add, Math.abs(now.type-1)));
-                    }
-                }
-            }
-
-            for(int i = 4; i<6; i++){
-                int nx = now.x + dx[now.type][i];
-                int ny = now.y + dy[now.type][i];
+            for(int i = 0; i<6; i++){
+                int nx = now.x + dx[(now.x+now.y)%2][i];
+                int ny = now.y + dy[(now.x+now.y)%2][i];
 
                 if(nx>=0 && ny>=0 && nx<m && ny<n){
-                    int add = (now.type==map[ny][nx]) ? 0 : 1;
-                    if(dp[ny][nx] > now.cnt + add){
-                        dp[ny][nx] = now.cnt + add;
+                    if(dp[ny][nx] > now.cnt + map[ny][nx]){
+                        dp[ny][nx] = now.cnt + map[ny][nx];
                         if(nx == m-1 && ny == n-1) return;
-                        pq.add(new Node(nx, ny, now.cnt+add, now.type));
+                        pq.add(new Node(nx, ny, dp[ny][nx]));
                     }
                 }
             }
         }
     }
-
 }
 //i+j is even, direction must be '\'
 //i+j is odd, direction must be '/'
