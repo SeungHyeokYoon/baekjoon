e8db2a17d11acab7b4e868bc04abb2b630c7f756
SeungHyeok Yun <162292720+SeungHyeokYoon@users.noreply.github.com>
2025-09-08 12:01:19 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -11,11 +11,12 @@ public class Main {
         int[] arr = new int[n+1];
 
         for(int i = 2; i<=n; i++){
-            if(i%3 == 0 && i%2 == 0) arr[i] = Math.min(Math.min(arr[i/3], arr[i/2]), arr[i-1]) + 1;
-            else if(i%3 == 0) arr[i] = Math.min(arr[i/3], arr[i-1]) + 1;
-            else if(i%2 == 0) arr[i] = Math.min(arr[i/2], arr[i-1]) + 1;
-            else arr[i] = arr[i-1]+1;
+            arr[i] = arr[i-1] + 1;
+            if (i%2 == 0) arr[i] = Math.min(arr[i], arr[i/2] + 1);
+            if (i%3 == 0) arr[i] = Math.min(arr[i], arr[i/3] + 1);
         }
+
         System.out.println(arr[n]);
+
     }
 }
