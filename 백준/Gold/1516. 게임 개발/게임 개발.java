894fca32a9d00418cc26801be9fae41fb6f395d4
SeungHyeok Yun <162292720+SeungHyeokYoon@users.noreply.github.com>
2025-09-10 11:51:32 +0900

    [Gold III] Title: 게임 개발, Time: 232 ms, Memory: 21748 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[] dist, time, indegree;
+    static ArrayList<Integer>[] map;
+
+
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+        dist = new int[n];
+        time = new int[n];
+        indegree = new int[n];
+        map = new ArrayList[n];
+        for(int i = 0; i<n; i++){
+            map[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int now = Integer.parseInt(st.nextToken());
+            time[i] = now;
+            while((now = Integer.parseInt(st.nextToken())) != -1){
+                indegree[i]++;
+                map[now-1].add(i);
+            }
+        }
+
+        Queue<Integer> q = new ArrayDeque<>();
+
+        for(int i = 0; i<n; i++){
+            if(indegree[i] == 0) q.add(i);
+            dist[i] = time[i];
+        }
+
+        while(!q.isEmpty()){
+            int now = q.poll();
+
+            for(Integer next : map[now]){
+                dist[next] = Math.max(dist[next], dist[now] + time[next]);
+                if(--indegree[next] == 0){
+                    q.add(next);
+                }
+            }
+        }
+
+        for(int i = 0; i<n; i++){
+            sb.append(dist[i]).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+
+}
